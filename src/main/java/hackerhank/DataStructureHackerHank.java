226611d9298b401c2f9b8335c1fa89d70f77625a
tiagowolsenmeli <tiago.wolsen@mercadolivre.com>
2023-11-25 18:14:37 -0300

    add HackerHank

---
+++ src/main/java/hackerhank/DataStructureHackerHank.java
@@ -0,0 +1,68 @@
+package hackerhank;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class DataStructureHackerHank {
+
+  public class Result {
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+      // Write your code here
+
+      List<Integer> newList = new ArrayList<>();
+ for(int i =0; i < a.size(); i ++){
+     Integer maxIndex = a.size() - i -1;
+     Integer temp = a.get(maxIndex);
+     newList.add(i, temp);
+     System.out.println(i);
+ }
+
+      return newList;
+    }
+
+  }
+
+  public class Solution {
+    public static void main(String[] args) throws IOException {
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+      int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+      List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+          .map(Integer::parseInt)
+          .collect(toList());
+
+      List<Integer> res = Result.reverseArray(arr);
+
+      bufferedWriter.write(
+          res.stream()
+              .map(Object::toString)
+              .collect(joining(" "))
+              + "\n"
+      );
+
+      bufferedReader.close();
+      bufferedWriter.close();
+    }
+  }
+
+}
---
+++ src/test/java/DataStructureHackerHankTest.java
@@ -0,0 +1,22 @@
+import hackerhank.DataStructureHackerHank;
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+public class DataStructureHackerHankTest {
+
+  @InjectMocks
+  private DataStructureHackerHank dataStructureHackerHank ;
+
+  @Test
+    public void test() throws IOException {
+        System.out.println("Hello world!");
+        List<Integer> initialList = List.of(1,2,3,4,5);
+        List<Integer> expected = List.of(5,4,3,2,1);
+        List<Integer> result = DataStructureHackerHank.Result.reverseArray(initialList);
+
+    Assertions.assertEquals(expected, result);
+    }
+}
