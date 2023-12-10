6160a932d6ed80e7d7182c5f4d1b931cb0438533
tiagowolsenmeli <tiago.wolsen@mercadolivre.com>
2023-12-10 18:15:06 -0300

    add

--- src/test/java/DataStructureHackerHankTest.java
+++ src/test/java/DataStructureHackerHankTest.java
@@ -3,8 +3,11 @@ import java.io.IOException;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class DataStructureHackerHankTest {
 
   @InjectMocks
