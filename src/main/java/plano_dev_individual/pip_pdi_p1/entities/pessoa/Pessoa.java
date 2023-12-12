8d25eace9cc14da4a139de927d1c4d48f2f8971f
tiagowolsenmeli <tiago.wolsen@mercadolivre.com>
2023-12-12 08:46:31 -0300

    abstract class

--- src/main/java/plano_dev_individual/pip_pdi_p1/entities/pessoa/Pessoa.java
+++ src/main/java/plano_dev_individual/pip_pdi_p1/entities/pessoa/Pessoa.java
@@ -1,7 +1,11 @@
 package plano_dev_individual.pip_pdi_p1.entities.pessoa;
 
-public interface Pessoa {
-  public long getId();
-  public long getExternalId();
-  public String getName();
+public abstract class Pessoa {
+  private long id;
+
+  public long getId() {
+    return id;
+  } //Herança
+  public abstract long getExternalId();  //Polimorfismo
+  public abstract String getName(); //Polimorfismo
 }
--- src/main/java/plano_dev_individual/pip_pdi_p1/entities/pessoa/PessoaFisica.java
+++ src/main/java/plano_dev_individual/pip_pdi_p1/entities/pessoa/PessoaFisica.java
@@ -2,7 +2,7 @@ package plano_dev_individual.pip_pdi_p1.entities.pessoa;
 
 import plano_dev_individual.pip_pdi_p1.entities.pessoa.sexualidade.Sexualidade;
 
-public class PessoaFisica implements Pessoa {
+public class PessoaFisica extends Pessoa {
   private long id;
   private String nome;
   private String sobrenome;
@@ -10,11 +10,6 @@ public class PessoaFisica implements Pessoa {
   private String rg;
   private String dataNascimento;
   private Sexualidade sexo;
-
-    @Override
-    public long getId() {
-      return id;
-    }
 
     @Override
     public long getExternalId() {
