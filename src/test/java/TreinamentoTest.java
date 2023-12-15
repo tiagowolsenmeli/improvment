062b07e395cc41dc4d856d11f835b6cc07b0468d
tiagowolsenmeli <tiago.wolsen@mercadolivre.com>
2023-12-15 14:49:18 -0300

    add treinamento inject mocks

--- src/test/java/TreinamentoTest.java
+++ src/test/java/TreinamentoTest.java
@@ -3,10 +3,14 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import plano_dev_individual.pip_pdi_p1.entities.charpessoas.conhecimento.Treinamento;
 
 public class TreinamentoTest {
 
+    @InjectMocks
+    private Treinamento treinamento;
 
   @Test
   public void testOrdenaListaTreinamentos() {
@@ -14,7 +18,7 @@ public class TreinamentoTest {
     final ArrayList<String>  expectedList = new ArrayList<>(Arrays.asList("C#", "C++", "Java", "Python"));
 
 
-    final ArrayList<String> result = Treinamento.ordenaListaTreinamentos(inputList);
+    final ArrayList<String> result = treinamento.ordenaListaTreinamentos(inputList);
 
     Assertions.assertEquals(expectedList, result);
   }
@@ -24,7 +28,17 @@ public class TreinamentoTest {
     final ArrayList<String> inputList = new ArrayList<>(Arrays.asList("Java", "C++", "Python", "C#"));
     final ArrayList<String>  expected = new ArrayList<>(Arrays.asList("C#", "C++", "Java", "Python"));
 
-    ArrayList<String> result = Treinamento.ordenaListaTreinamentos(inputList);
+    ArrayList<String> result = treinamento.ordenaListaTreinamentos(inputList);
+
+    Assertions.assertEquals(expected, result);
+  }
+
+  @Test
+  public void testOrdenaListaTreinamentosCompareTo() {
+    final ArrayList<String> inputList = new ArrayList<>(Arrays.asList("Java", "C++", "Python", "C#"));
+    final ArrayList<String>  expected = new ArrayList<>(Arrays.asList("C#", "C++", "Java", "Python"));
+
+    ArrayList<String> result = treinamento.ordenaCompateTo(inputList);
 
     Assertions.assertEquals(expected, result);
   }
@@ -32,7 +46,18 @@ public class TreinamentoTest {
   @Test
   public void testOrdenaListaTreinamentosCompareTo2() {
     final ArrayList<String> inputList = new ArrayList<>(Arrays.asList("Java", "C++", "Python", "C#"));
-    final ArrayList<String>  expected = new ArrayList<>(Arrays.asList("C#", "C++", "Java", "Python"));
+    inputList.add("Claudio");
+    inputList.add("Zaza");
+    inputList.add("Rodrigo");
+    inputList.add("Washington");
+    inputList.add("Barbara");
+    inputList.add("Ellen");
+    inputList.add("Ângstron");
+    inputList.add("Óstia");
+    inputList.add("1234555");
+
+    final ArrayList<String>  expected = new ArrayList<>(Arrays.asList("1234555", "Barbara", "C#", "C++", "Claudio", "Ellen",
+        "Java", "Python", "Rodrigo", "Washington", "Zaza", "Ângstron", "Óstia"));
 
     ArrayList<String> result = Treinamento.ordenaCompateTo(inputList);
 
@@ -55,7 +80,7 @@ public class TreinamentoTest {
     final ArrayList<String>  expected = new ArrayList<>(Arrays.asList("1234555", "Barbara", "C#", "C++", "Claudio", "Ellen",
         "Java", "Python", "Rodrigo", "Washington", "Zaza", "Ângstron", "Óstia"));
 
-    ArrayList<String> result = Treinamento.ordenaListaTreinamentos(inputList);
+    ArrayList<String> result = treinamento.ordenaListaTreinamentos(inputList);
 
     Assertions.assertEquals(expected, result);
   }
