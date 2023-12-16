d78ca739015098a40a7b57b3736ede2ceb9d990e
tiagowolsenmeli <tiago.wolsen@mercadolivre.com>
2023-12-16 17:28:25 -0300

    resolve conflicts

--- src/main/java/plano_dev_individual/pip_pdi_p1/entities/pessoa/Pessoa.java
+++ src/main/java/plano_dev_individual/pip_pdi_p1/entities/pessoa/Pessoa.java
@@ -1,6 +1,5 @@
 package plano_dev_individual.pip_pdi_p1.entities.pessoa;
 
-<<<<<<< HEAD
 public abstract class Pessoa {
   private long id;
 
@@ -9,10 +8,5 @@ public abstract class Pessoa {
   } //Herança
   public abstract long getExternalId();  //Polimorfismo
   public abstract String getName(); //Polimorfismo
-=======
-public interface Pessoa {
-  public long getId();
-  public long getExternalId();
-  public String getName();
->>>>>>> a0b91a8 (add pip_pdi_p1)
+
 }
--- src/main/java/plano_dev_individual/pip_pdi_p1/entities/pessoa/PessoaFisica.java
+++ src/main/java/plano_dev_individual/pip_pdi_p1/entities/pessoa/PessoaFisica.java
@@ -2,11 +2,7 @@ package plano_dev_individual.pip_pdi_p1.entities.pessoa;
 
 import plano_dev_individual.pip_pdi_p1.entities.pessoa.sexualidade.Sexualidade;
 
-<<<<<<< HEAD
 public class PessoaFisica extends Pessoa {
-=======
-public class PessoaFisica implements Pessoa {
->>>>>>> a0b91a8 (add pip_pdi_p1)
   private long id;
   private String nome;
   private String sobrenome;
@@ -16,14 +12,11 @@ public class PessoaFisica implements Pessoa {
   private Sexualidade sexo;
 
     @Override
-<<<<<<< HEAD
-=======
     public long getId() {
       return id;
     }
 
     @Override
->>>>>>> a0b91a8 (add pip_pdi_p1)
     public long getExternalId() {
       return cpf;
     }
