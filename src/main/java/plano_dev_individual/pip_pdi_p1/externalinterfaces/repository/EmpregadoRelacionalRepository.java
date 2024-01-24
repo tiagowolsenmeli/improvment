aa114211232a9de92bc9e84c01ba32ddebc05862
tiagowolsenmeli <tiago.wolsen@mercadolivre.com>
2024-01-24 14:36:28 -0300

    init Listarfuncionarios

--- src/main/java/plano_dev_individual/pip_pdi_p1/externalinterfaces/repository/EmpregadoRelacionalRepository.java
+++ src/main/java/plano_dev_individual/pip_pdi_p1/externalinterfaces/repository/EmpregadoRelacionalRepository.java
@@ -1,23 +1,37 @@
 package plano_dev_individual.pip_pdi_p1.externalinterfaces.repository;
 
-import java.util.HashSet;
 import java.util.Set;
 import plano_dev_individual.pip_pdi_p1.entities.Funcionario;
 import plano_dev_individual.pip_pdi_p1.repositories.EmpregadoRelacionalBD;
 
 public class EmpregadoRelacionalRepository implements EmpregadoRelacionalBD {
 
-  private Set<Funcionario> funcionarios;
+  private Set<Funcionario> funcionariosAtivos;
+
+  private Set<Funcionario> funcionariosInaptos;
 
     public EmpregadoRelacionalRepository() {
       Funcionario funcionario = new Funcionario();
       funcionario.setCpf("12345678901");
-        funcionarios.add(funcionario);
+      Funcionario funcionario2 = new Funcionario();
+      funcionario2.setCpf("12345678902");
+        funcionariosAtivos.add(funcionario);
+        funcionariosAtivos.add(funcionario2);
+        funcionariosAtivos.add(funcionario2);
+
+      Funcionario funcionario3 = new Funcionario();
+      funcionario3.setCpf("12345678903");
+      Funcionario funcionario4 = new Funcionario();
+      funcionario4.setCpf("12345678904");
+      funcionariosAtivos.add(funcionario3);
+      funcionariosAtivos.add(funcionario4);
+      funcionariosAtivos.add(funcionario3);
+
     }
 
   @Override
   public Set<Funcionario> getFuncionariosAptosParaSalario() {
-    return  funcionarios;
+    return funcionariosAtivos;
   }
 
   @Override
