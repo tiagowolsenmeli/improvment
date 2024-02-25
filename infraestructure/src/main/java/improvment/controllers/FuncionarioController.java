0fece17f76e61506a70e3241cb79505c3852c3a6
tiagowolsenmeli <tiago.wolsen@mercadolivre.com>
2024-02-25 13:51:38 -0300

    add Salario Controller with JPA

--- infraestructure/src/main/java/improvment/controllers/FuncionarioController.java
+++ infraestructure/src/main/java/improvment/controllers/FuncionarioController.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
