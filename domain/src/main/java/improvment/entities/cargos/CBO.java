4aa6974e7ce2aba8755dd3ed63c5ce5f7e2ab91d
tiagowolsenmeli <tiago.wolsen@mercadolivre.com>
2024-03-07 14:13:38 -0300

    add

--- domain/src/main/java/improvment/entities/cargos/CBO.java
+++ domain/src/main/java/improvment/entities/cargos/CBO.java
@@ -1,9 +1,16 @@
 package improvment.entities.cargos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class CBO {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
     private String nome;
     private String descricao;
